8beba14ef488249eea23273908d2da557d78421b
Mukul-Mano <162026904+Mukul-Mano@users.noreply.github.com>
2024-03-03 18:36:49 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -19,9 +19,9 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
 ENV = bool(os.environ.get('ENV', False))
 
 if ENV:
-    TOKEN = os.environ.get('TOKEN', None)
+    TOKEN = os.environ.get('7084346622:AAF2b4ShPrMOYEExNN_Gcp-BVhJXxG3GOM0', None)
     try:
-        OWNER_ID = int(os.environ.get('OWNER_ID', None))
+        OWNER_ID = int(os.environ.get('KING', None))
     except ValueError:
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
